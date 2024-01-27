6d6034bb671daace93e2690308e0d78d3100a278
zijaigidov <igidov.zija@gmail.com>
2024-01-27 21:35:15 +0100

    Add global symbolic constants for the math operations
    NOTE: I added two variables for the multiplication operation (one for '*', one for 'x'). It would be best to "merge" these two variables into one, but I don't yet know how that can be accomplished.

---
+++ math.h
@@ -0,0 +1,16 @@
+// math.h
+
+#ifndef MATH_H
+#define MATH_H
+
+namespace MathOperations
+{
+	inline constexpr char addition            { '+' };
+	inline constexpr char subtraction         { '-' };
+	inline constexpr char multiplicationStar  { '*' };
+	inline constexpr char multiplicationCross { 'x' };
+	inline constexpr char division            { '/' };
+	inline constexpr char modulo              { '%' };
+}
+
+#endif
